09614e84219fbff09b6a0cc3d8a7dffaf2f4af50
AlexMTeixeira <anguinho95@gmail.com>
2019-12-03 11:14:56 +0000

    Generator

--- Tarefa_3/generator/generator.hs
+++ Tarefa_3/generator/generator.hs
@@ -42,37 +42,34 @@ genEmail = do a <- genPrefix' 8
 genMorada :: Gen Morada
 genMorada = elements moradas
 
-genProp :: Int -> Gen [Prop]
-genProp 0 = return []
-genProp n = do a <- vectorOf n $ genNome
-               b <- vectorOf n $ genNif
-               c <- vectorOf n $ genEmail 
-               d <- vectorOf n $ genMorada 
-               --return []
-               return (genProp' a b c d)
+genProp :: Int -> [String] -> Gen [Prop]
+genProp 0 _ = return []
+genProp n b = do a <- vectorOf n $ genNome
+                 c <- vectorOf n $ genEmail 
+                 d <- vectorOf n $ genMorada 
+                 --return []
+                 return (genProp' a b c d)
 
 
 --NovoProp: nome, nif, email,morada
 genProp' :: [Nome] -> [Nif] -> [Email] -> [Morada] -> [Prop]
 genProp' [] _ _ _ = []
-genProp' (a:as) (b:bs) (c:cs) (d:ds) = [("NovoProp:"++a++":"++b++":"++c++":"++d)]++(genProp' as bs cs ds)
+genProp' (a:as) (b:bs) (c:cs) (d:ds) = [("NovoProp:"++a++","++b++","++c++","++d)]++(genProp' as bs cs ds)
 
-genCliente :: Int -> Gen [Cliente]
-genCliente 0 = return []
-genCliente n = do a <- vectorOf n $ genNome
-                  b <- vectorOf n $ genNif
-                  c <- vectorOf n $ genEmail 
-                  d <- vectorOf n $ genMorada 
-                  x <- vectorOf n $ choose ((-9.32),(-6.32))
-                  y <- vectorOf n $ choose ((37.0),(42.0))
-                  return (genCliente' a b c d x y)
+genCliente :: Int -> [String] -> Gen [Cliente]
+genCliente 0 _ = return []
+genCliente n b = do a <- vectorOf n $ genNome
+                    c <- vectorOf n $ genEmail 
+                    d <- vectorOf n $ genMorada 
+                    x <- vectorOf n $ choose ((-9.32),(-6.32))
+                    y <- vectorOf n $ choose ((37.0),(42.0))
+                    return (genCliente' a b c d x y)
 
 --NovoCliente:nome,nif,email,morada,X,Y
 genCliente' :: [Nome] -> [Nif] -> [Email] -> [Morada] -> [Float] -> [Float] -> [Cliente]
 genCliente' [] _ _ _ _ _ = []
-genCliente' (a:as) (b:bs) (c:cs) (d:ds) (x:xs) (y:ys) = [("NovoCliente:"++a++":"++b++":"++c++":"++d++":"++(show x)++":"++(show y))]++(genCliente' as bs cs ds xs ys)
+genCliente' (a:as) (b:bs) (c:cs) (d:ds) (x:xs) (y:ys) = [("NovoCliente:"++a++","++b++","++c++","++d++","++(show x)++","++(show y))]++(genCliente' as bs cs ds xs ys)
 
---NovoCarro:tipo,marca,matricula,nif,velocidade media,preço por km, consumo por km, autonomia, X, Y
 genTipo :: Gen String
 genTipo = frequency [(9900,return "Gasolina"),(800,return "Hibrido"),(400,return "Eletrico")]
 
@@ -100,16 +97,76 @@ genConsumo = choose (1.0000,4)
 genAutonomia :: Gen Int
 genAutonomia = frequency [(70,(elements [450..600])),(10,(elements [600..900])),(20,(elements [350..450]))]
 
+--NovoCarro:tipo,marca,matricula,nif,velocidade media,preço por km, consumo por km, autonomia, X, Y
+genCarro :: Int -> [String] -> [String] -> Gen [String]
+genCarro 0 _ _ = return []
+genCarro n c nifs = do a <- vectorOf n $ genTipo
+                       b <- vectorOf n $ genMarca
+                       d <- shuffle $ (nifs++nifs++nifs++nifs++nifs++nifs++nifs++nifs++nifs++nifs++nifs++nifs)
+                       e <- vectorOf n $ genVelocidade
+                       f <- vectorOf n $ genPreco
+                       g <- vectorOf n $ genConsumo
+                       h <- vectorOf n $ genAutonomia
+                       x <- vectorOf n $ choose ((-9.32),(-6.32))
+                       y <- vectorOf n $ choose ((37.0),(42.0))
+                       return (genCarro' a b c d e f g h x y)
+
+getNif :: [String] -> Gen String
+getNif n = elements n
+
+genCarro' :: [String] -> [String] -> [String] -> [String] -> [Int] -> [Float] -> [Float] -> [Int] -> [Float] -> [Float] -> [String]
+genCarro' [] _ _ _ _ _ _ _ _ _ = []
+genCarro' (a:as) (b:bs) (c:cs) (d:ds) (e:es) (f:fs) (g:gs) (h:hs) (x:xs) (y:ys) = [("NovoCarro:"++a++","++b++","++c++","++d++","++(show e)++","++(show f)++","++(show g)++","++(show h)++","++(show x)++","++(show y))]++(genCarro' as bs cs ds es fs gs hs xs ys)
 --Aluguer: nif cliente, X destino, Y destino, tipoCombustivel , preferencia
 genPerferencia :: Gen String
 genPerferencia = elements ["MaisPerto","MaisBarato"]
+
+genAluguer :: Int -> [String] -> Gen [String]
+genAluguer 0 _ = return []
+genAluguer n nifs = do a <- shuffle $ (nifs++nifs++nifs++nifs)                      
+                       x <- vectorOf n $ choose ((-9.32),(-6.32))
+                       y <- vectorOf n $ choose ((37.0),(42.0))
+                       b <- vectorOf n $ genTipo
+                       c <- vectorOf n $ genPerferencia
+                       return (genAluguer' a x y b c)
+
+genAluguer' :: [String] -> [Float] -> [Float] -> [String] -> [String] -> [String]
+genAluguer' _ [] _ _ _ = []
+genAluguer' (a:as) (x:xs) (y:ys) (b:bs) (c:cs) = [("Aluguer:"++a++","++(show x)++","++(show y)++","++b++","++c)]++(genAluguer' as xs ys bs cs)
 
 --Classificar: matricula ou nif (cliente ou prop) , nota (0-100)
 genNota :: Gen Int
 genNota = frequency [(80,(elements [50..80])),(1,(elements [0..40])),(4,(elements [41..49])),(15,(elements [81..100]))]
 
-genLogs :: Int -> Gen [String]
-genLogs n = do a <- vectorOf n $ genNif 
+genClassificar :: Int -> [String] -> [String] -> Gen [String]
+genClassificar 0 _ _ = return []
+genClassificar n mat nifs = do a <- shuffle (mat++nifs)
+                               b <- vectorOf n $ genNota
+                               return (genClassificar' a b)
+
+genClassificar' :: [String] -> [Int] -> [String]
+genClassificar' _ [] = []
+genClassificar' (a:as) (b:bs) = ["Classificar:"++a++","++(show b)]++(genClassificar' as bs)
+
+--genLogs :: Int -> Gen [String]
+--genLogs n = do nifs <- vectorOf n $ genNif 
+
+main = do nifProps <- generate $ vectorOf 200 $ genNif 
+          nifClientes <- generate $ vectorOf 600 $ genNif
+          props <- generate $ genProp 200 nifProps
+          clients <- generate $ genCliente 600 nifClientes
+          matriculas <- generate $ vectorOf 2000 $ genMatricula
+          cars <- generate $ genCarro 2000 matriculas nifProps
+          alu <- generate $ genAluguer 500 nifClientes
+          clas <- generate $ genClassificar 2000 matriculas nifProps
+          mapM_ putStrLn props
+          mapM_ putStrLn clients
+          mapM_ putStrLn cars
+          mapM_ putStrLn alu
+          mapM_ putStrLn clas
+
+
+
 
 delete element list = filter (\e -> e/=element) list
 
